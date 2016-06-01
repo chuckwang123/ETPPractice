5381354efdd75a48dfe31938e41f05438bbeb5c2
chuckwang123 <wangjohn419@gmail.com>
2016-06-01 00:08:01 -0500

    make the button worked

--- ETPPractice/ETPPractice.Models/ContactCategoryInfo.cs
+++ ETPPractice/ETPPractice.Models/ContactCategoryInfo.cs
@@ -2,6 +2,7 @@
 {
     public class ContactCategoryInfo
     {
+        public int Id { get; set; }
         public string Role { get; set; }
         public int Item_Order { get; set; }
     }
--- ETPPractice/ETPPractice/scripts/CheckListJsp.js
+++ ETPPractice/ETPPractice/scripts/CheckListJsp.js
@@ -14,7 +14,30 @@ $(document).ready(function () {
 
 
 function AddRow(item) {
-    var newRowContent = "<tr><td>" + item.Role + "</td><td><input type='text'' name='name'></td>" +
-        "<td><input type='text'' name='email'></td><td><input type='text'' name='org_Position'></td>";
+    var newRowContent = "<tr id=" + item.Id + "><td>" + item.Role + "</td><td><input type='text'' name='name'></td>" +
+        "<td><input type='text'' name='email'></td><td><input type='text'' name='email'></td>";
     $('#RoleTable > tbody:last-child').append(newRowContent);
 }
+
+$('#submit').click(function() {
+    $('#RoleTable > tbody  > tr').each(function() {
+        if ($(this).find('[name="name"]').val() === '') {
+            return ;
+        }
+        var contactInfo = {
+            checkList_id: 1,
+            role_ID: $(this).attr('Id'),
+            Name: $(this).find('[name="name"]').val(),
+            Email: $(this).find('[name="email"]').val(),
+            org_position: $(this).find('[name="email"]').val()
+        };
+        $.ajax({
+            url: '/api/Contact',
+            type: 'post',
+            dataType: 'json',
+            success: function (data) {
+            },
+            data: contactInfo
+        });
+    });  
+})
