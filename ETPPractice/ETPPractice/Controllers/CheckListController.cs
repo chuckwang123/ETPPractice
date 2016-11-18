17152cf14d9d568629a588c2c0fbd8dba610f2f1
Junyu Wang <JWang@imo-online.com>
2016-11-18 17:17:47 -0600

    Add more

--- ETPPractice/ETPPractice/Controllers/CheckListController.cs
+++ ETPPractice/ETPPractice/Controllers/CheckListController.cs
@@ -2,12 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
-using Amazon.CloudWatchLogs;
-using Amazon.Runtime;
 using ETPPractice.Models;
 using ETPPractice.Util;
-using Serilog;
-using Serilog.Sinks.AwsCloudWatch;
 
 namespace ETPPractice.Controllers
 {
